34fd0e5e7f544687cb0da15737b8d07d5ddda503
Matteo Barone <matteo.barone1199@gmail.com>
2026-02-01 23:00:27 +0100

    added security config

--- src/main/java/com/character_almanach/config/SecurityConfiguration.java
+++ src/main/java/com/character_almanach/config/SecurityConfiguration.java
@@ -7,7 +7,9 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import com.character_almanach.filter.JwtAuthFilter;
@@ -21,12 +23,17 @@ public class SecurityConfiguration {
 
     private final JwtAuthFilter jwtAuthenticationFilter;
     private final AuthenticationProvider authenticationProvider;
+    private AuthenticationEntryPoint jwtAuthenticationEntryPoint;
+    private AccessDeniedHandler jwtAccessDeniedHandler;
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable)
             .sessionManagement(session ->
                 session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            ).exceptionHandling(ex -> ex
+                .authenticationEntryPoint(jwtAuthenticationEntryPoint)
+                .accessDeniedHandler(jwtAccessDeniedHandler)
             )
             .authenticationProvider(authenticationProvider)
             .addFilterBefore(
