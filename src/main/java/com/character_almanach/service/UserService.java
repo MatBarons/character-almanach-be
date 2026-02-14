c15bc2f0e35b173c3be04daa48d647f57b23255e
Matteo Barone <matteo.barone1199@gmail.com>
2026-02-14 16:29:44 +0100

    removed import

--- src/main/java/com/character_almanach/service/UserService.java
+++ src/main/java/com/character_almanach/service/UserService.java
@@ -1,7 +1,6 @@
 package com.character_almanach.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
